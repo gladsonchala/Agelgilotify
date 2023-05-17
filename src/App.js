79ab5eec6d6e55ee6d2b39a7349e4b2b055acc5d
gemechischala <66429401+gemechischala@users.noreply.github.com>
2023-05-17 22:02:27 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import UserDashboard from './components/User/UserDashboard';
+import AdminDashboard from './components/Admin/AdminDashboard';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/admin" component={AdminDashboard} />
+          <Route path="/" component={UserDashboard} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
