4521ff5abca9a0fc89d575b5ae94944637cfaac9
gemechischala <66429401+gemechischala@users.noreply.github.com>
2023-05-17 22:18:11 +0300

    Create AdminPanel.js

---
+++ src/components/Admin/AdminPanel.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function AdminPanel() {
+  const [hotels, setHotels] = useState([]);
+
+  const handleAddHotel = () => {
+    // Logic to add a new hotel to the list
+    const newHotel = {
+      name: 'New Hotel',
+      location: 'New Location',
+      priceRange: '100-500',
+      photos: [],
+    };
+    setHotels([...hotels, newHotel]);
+  };
+
+  const handleDeleteHotel = (index) => {
+    // Logic to delete a hotel from the list
+    const updatedHotels = [...hotels];
+    updatedHotels.splice(index, 1);
+    setHotels(updatedHotels);
+  };
+
+  return (
+    <div>
+      <h2>Admin Panel</h2>
+      <button onClick={handleAddHotel}>Add Hotel</button>
+      <ul>
+        {hotels.map((hotel, index) => (
+          <li key={index}>
+            <div>
+              <h3>{hotel.name}</h3>
+              <p>Location: {hotel.location}</p>
+              <p>Price Range: {hotel.priceRange}</p>
+              <button onClick={() => handleDeleteHotel(index)}>Delete Hotel</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AdminPanel;
