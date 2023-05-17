5547ba6222c07b2a17e116eade991547fac15b76
gemechischala <66429401+gemechischala@users.noreply.github.com>
2023-05-17 22:07:27 +0300

    Create Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // TODO: Implement login functionality using email and password
+    // You can use Firebase authentication or any other authentication method of your choice
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
