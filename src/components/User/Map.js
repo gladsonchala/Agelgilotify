704005d0a929ab00ee3f98487bdbffdf7041703a
gemechischala <66429401+gemechischala@users.noreply.github.com>
2023-05-17 22:15:07 +0300

    Create Map.js

---
+++ src/components/User/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Map({ hotels }) {
+  // Implement map functionality using Google Maps API or any other map library of your choice
+
+  return (
+    <div>
+      <h2>Map</h2>
+      {/* Render the map component here */}
+    </div>
+  );
+}
+
+export default Map;
