9a593ce78b00ea5a44fe2015ea751304e730a920
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:37:28 +0300

    Update HotelList.js

--- src/components/User/HotelList.js
+++ src/components/User/HotelList.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 function HotelList({ hotels }) {
   return (
-    <div>
-      <h2>Hotels</h2>
+    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>Hotels</h2>
       {hotels.map((hotel) => (
-        <div key={hotel.id}>
-          <h3>{hotel.name}</h3>
-          <p>Location: {hotel.location}</p>
-          <p>Price Range: {hotel.priceRange}</p>
+        <div
+          key={hotel.id}
+          style={{
+            marginBottom: '20px',
+            padding: '20px',
+            background: '#f8f8f8',
+            borderRadius: '5px',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)'
+          }}
+        >
+          <h3 style={{ fontSize: '20px', marginBottom: '10px' }}>{hotel.name}</h3>
+          <p style={{ marginBottom: '5px' }}>Location: {hotel.location}</p>
+          <p style={{ marginBottom: '5px' }}>Price Range: {hotel.priceRange}</p>
           {/* Render additional hotel details as needed */}
         </div>
       ))}
