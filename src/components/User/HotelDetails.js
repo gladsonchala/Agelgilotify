1c22d4a03a8f227285749764b7e7a6f2c820e7a4
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:38:03 +0300

    Update HotelDetails.js

--- src/components/User/HotelDetails.js
+++ src/components/User/HotelDetails.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 function HotelDetails({ hotel }) {
   return (
-    <div>
-      <h2>{hotel.name}</h2>
-      <p>Location: {hotel.location}</p>
-      <p>Price Range: {hotel.priceRange}</p>
+    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>{hotel.name}</h2>
+      <p style={{ marginBottom: '10px' }}>Location: {hotel.location}</p>
+      <p style={{ marginBottom: '10px' }}>Price Range: {hotel.priceRange}</p>
       <div>
-        <h3>Photos:</h3>
-        {hotel.photos.map((photo) => (
-          <img key={photo.id} src={photo.url} alt={`Photo of ${hotel.name}`} />
-        ))}
+        <h3 style={{ fontSize: '20px', marginBottom: '10px' }}>Photos:</h3>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {hotel.photos.map((photo) => (
+            <img
+              key={photo.id}
+              src={photo.url}
+              alt={`Photo of ${hotel.name}`}
+              style={{ width: '200px', height: '150px', objectFit: 'cover', marginRight: '10px', marginBottom: '10px' }}
+            />
+          ))}
+        </div>
       </div>
       {/* Render additional hotel details and functionalities as needed */}
     </div>
