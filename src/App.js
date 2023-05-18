19572eb0f75f86e2a4b1bf3ec89ef6208432c42e
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:33:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,64 @@ import AdminDashboard from './components/Admin/AdminDashboard';
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Header />
+      <div
+        className="app"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+          backgroundColor: '#f5f5f5',
+          fontFamily: 'Arial, sans-serif',
+        }}
+      >
+        <Header
+          style={{
+            backgroundColor: '#1A237E',
+            color: '#ffffff',
+            padding: '20px',
+            textAlign: 'center',
+          }}
+        />
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
+          <Route
+            path="/login"
+            component={() => (
+              <Login
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100vh',
+                  fontSize: '20px',
+                }}
+              />
+            )}
+          />
+          <Route
+            path="/signup"
+            component={() => (
+              <Signup
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100vh',
+                  fontSize: '20px',
+                }}
+              />
+            )}
+          />
           <Route path="/admin" component={AdminDashboard} />
           <Route path="/" component={UserDashboard} />
         </Switch>
-        <Footer />
+        <Footer
+          style={{
+            backgroundColor: '#1A237E',
+            color: '#ffffff',
+            padding: '20px',
+            textAlign: 'center',
+          }}
+        />
       </div>
     </Router>
   );
