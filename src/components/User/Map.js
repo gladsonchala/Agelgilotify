81ffed116f156a7d9c9303bb4cc80630c8ba3f00
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 09:16:53 +0300

    Update Map.js

--- src/components/User/Map.js
+++ src/components/User/Map.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+// Google Maps API script URL
+const MAPS_API_URL = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places`;
 
 function Map({ hotels }) {
-  // Implement map functionality using Google Maps API or any other map library of your choice
+  const mapContainerRef = useRef(null);
+  let map;
+
+  useEffect(() => {
+    // Load Google Maps API script
+    const loadMapScript = () => {
+      const script = document.createElement('script');
+      script.src = MAPS_API_URL;
+      script.async = true;
+      script.defer = true;
+      script.addEventListener('load', initializeMap);
+      document.body.appendChild(script);
+    };
+
+    // Initialize the map
+    const initializeMap = () => {
+      const mapOptions = {
+        center: { lat: 0, lng: 0 }, // Set the initial center of the map
+        zoom: 12, // Set the initial zoom level
+      };
+      map = new window.google.maps.Map(mapContainerRef.current, mapOptions);
+    };
+
+    // Load the map script and initialize the map
+    loadMapScript();
+
+    // Clean up the map when the component unmounts
+    return () => {
+      if (map) {
+        map = null;
+      }
+    };
+  }, []);
 
   return (
     <div style={{ maxWidth: '600px', margin: '0 auto' }}>
       <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>Map</h2>
-      {/* Render the map component here */}
+      <div ref={mapContainerRef} style={{ height: '400px' }}></div>
     </div>
   );
 }
