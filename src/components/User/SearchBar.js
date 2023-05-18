a9ba62f671ed540184e44bc4b6f36417f1f75d74
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:36:51 +0300

    Update SearchBar.js

--- src/components/User/SearchBar.js
+++ src/components/User/SearchBar.js
@@ -9,15 +9,37 @@ function SearchBar({ handleSearch }) {
   };
 
   return (
-    <div>
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search by city..."
           value={city}
           onChange={(e) => setCity(e.target.value)}
+          style={{
+            width: '100%',
+            padding: '10px',
+            fontSize: '16px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            marginBottom: '10px'
+          }}
         />
-        <button type="submit">Search</button>
+        <button
+          type="submit"
+          style={{
+            display: 'block',
+            width: '100%',
+            padding: '10px',
+            background: '#333',
+            color: '#fff',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer'
+          }}
+        >
+          Search
+        </button>
       </form>
     </div>
   );
