7820e852b26589300703f301f10796f80e385e26
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:34:53 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Agelgilotify. All rights reserved.</p>
+    <footer style={{ background: '#333', color: '#fff', padding: '10px', textAlign: 'center' }}>
+      <p style={{ margin: 0, fontSize: '14px' }}>
+        &copy; {new Date().getFullYear()} Agelgilotify. All rights reserved.
+      </p>
     </footer>
   );
 }
