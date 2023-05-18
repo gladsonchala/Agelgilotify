697e6a0dbaf252680d1c025faa28621a6ecc7ed1
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 09:11:40 +0300

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,57 @@
 import React, { useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const handleSignup = async (e) => {
+  e.preventDefault();
+
+  try {
+    // Create user with email and password
+    const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
+
+    // Add additional user data to the database
+    const userData = {
+      name: name, // Replace 'name' with the variable holding the user's name
+      isAdmin: false, // Set 'isAdmin' to false by default
+    };
+
+    // Store user data in the database
+    await firebase.database().ref('users/' + user.uid).set(userData);
+
+    // Signup successful, you can redirect to another page or perform additional actions
+  } catch (error) {
+    // Handle signup error
+    console.log(error);
+  }
+};
 
 function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
-  const handleSignup = (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
-    // TODO: Implement signup functionality using email and password
-    // You can use Firebase authentication or any other authentication method of your choice
+
+    try {
+      // Create user with email and password
+      const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
+
+      // Add additional user data to the database
+      const userData = {
+        name: name,
+        isAdmin: false,
+      };
+
+      // Store user data in the database
+      await firebase.database().ref('users/' + user.uid).set(userData);
+
+      // Signup successful, you can redirect to another page or perform additional actions
+    } catch (error) {
+      // Handle signup error
+      console.log(error);
+    }
   };
 
   return (
@@ -15,17 +59,15 @@ function Signup() {
       <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Signup</h2>
       <form onSubmit={handleSignup}>
         <div style={{ marginBottom: '10px' }}>
-          <label style={{ display: 'block', marginBottom: '5px' }}>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ width: '100%', padding: '5px' }} />
+          <label style={{ display: 'block', marginBottom: '5px' }}>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ width: '100%', padding: '5px' }}
+            required
+          />
         </div>
-        <div style={{ marginBottom: '10px' }}>
-          <label style={{ display: 'block', marginBottom: '5px' }}>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ width: '100%', padding: '5px' }} />
-        </div>
-        <button type="submit" style={{ display: 'block', width: '100%', padding: '10px', background: '#333', color: '#fff', border: 'none' }}>Signup</button>
-      </form>
-    </div>
-  );
-}
+        <div style={{ marginBottom: '10px'
 
 export default Signup;
