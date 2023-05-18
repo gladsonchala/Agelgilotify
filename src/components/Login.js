9aeddd672c7ea1572153d8bb49277a1ca80b91db
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 09:09:03 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const handleLogin = async (e) => {
+  e.preventDefault();
+
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+    // Login successful, you can redirect to another page or perform additional actions
+  } catch (error) {
+    // Handle login error
+    console.log(error);
+  }
+};
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // TODO: Implement login functionality using email and password
-    // You can use Firebase authentication or any other authentication method of your choice
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      // Login successful, you can redirect to another page or perform additional actions
+    } catch (error) {
+      // Handle login error
+      console.log(error);
+    }
   };
 
   return (
@@ -16,13 +36,35 @@ function Login() {
       <form onSubmit={handleLogin}>
         <div style={{ marginBottom: '10px' }}>
           <label style={{ display: 'block', marginBottom: '5px' }}>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ width: '100%', padding: '5px' }} />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ width: '100%', padding: '5px' }}
+          />
         </div>
         <div style={{ marginBottom: '10px' }}>
           <label style={{ display: 'block', marginBottom: '5px' }}>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ width: '100%', padding: '5px' }} />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ width: '100%', padding: '5px' }}
+          />
         </div>
-        <button type="submit" style={{ display: 'block', width: '100%', padding: '10px', background: '#333', color: '#fff', border: 'none' }}>Login</button>
+        <button
+          type="submit"
+          style={{
+            display: 'block',
+            width: '100%',
+            padding: '10px',
+            background: '#333',
+            color: '#fff',
+            border: 'none',
+          }}
+        >
+          Login
+        </button>
       </form>
     </div>
   );
