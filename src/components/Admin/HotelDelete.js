9590ef6ab57369d345eda2d859fc4a580313c593
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:42:54 +0300

    Update HotelDelete.js

--- src/components/Admin/HotelDelete.js
+++ src/components/Admin/HotelDelete.js
@@ -6,10 +6,22 @@ function HotelDelete({ hotel, deleteHotel }) {
   };
 
   return (
-    <div>
-      <h2>Delete Hotel</h2>
-      <p>Are you sure you want to delete {hotel.name}?</p>
-      <button onClick={handleDelete}>Delete</button>
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+      <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>Delete Hotel</h2>
+      <p style={{ marginBottom: '10px' }}>Are you sure you want to delete {hotel.name}?</p>
+      <button
+        onClick={handleDelete}
+        style={{
+          padding: '8px 16px',
+          backgroundColor: '#ff0000',
+          color: 'white',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer',
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 }
