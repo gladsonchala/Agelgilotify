7ec37f8e36cab483fac715e0d1f4f3d7038a5fbd
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:41:39 +0300

    Update HotelList.js

--- src/components/Admin/HotelList.js
+++ src/components/Admin/HotelList.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
-function HotelList({ hotels, deleteHotel }) {
+function HotelList({ hotels, deleteHotel, editHotel }) {
   return (
-    <div>
-      <h2>Hotel List</h2>
-      <ul>
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+      <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>Hotel List</h2>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
         {hotels.map((hotel, index) => (
-          <li key={index}>
-            <div>
-              <h3>{hotel.name}</h3>
-              <p>Location: {hotel.location}</p>
-              <p>Price Range: {hotel.priceRange}</p>
-              <button onClick={() => deleteHotel(index)}>Delete Hotel</button>
+          <li key={index} style={{ marginBottom: '20px' }}>
+            <div style={{ backgroundColor: '#f5f5f5', padding: '10px', borderRadius: '4px' }}>
+              <h3 style={{ marginBottom: '5px' }}>{hotel.name}</h3>
+              <p style={{ marginBottom: '5px' }}>Location: {hotel.location}</p>
+              <p style={{ marginBottom: '10px' }}>Price Range: {hotel.priceRange}</p>
+              <div>
+                <button
+                  onClick={() => editHotel(index)}
+                  style={{
+                    padding: '8px 16px',
+                    backgroundColor: '#2196f3',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    cursor: 'pointer',
+                    marginRight: '10px',
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => deleteHotel(index)}
+                  style={{
+                    padding: '8px 16px',
+                    backgroundColor: '#f44336',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    cursor: 'pointer',
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
           </li>
         ))}
