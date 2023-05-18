d7afe6b24d01b434b77e7f16625d7f507a8520b8
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 09:14:30 +0300

    Update HotelList.js

--- src/components/User/HotelList.js
+++ src/components/User/HotelList.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
 
-function HotelList({ hotels }) {
+function HotelList() {
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    // Fetch hotels from the database
+    const fetchHotels = async () => {
+      try {
+        const snapshot = await firebase.database().ref('hotels').once('value');
+        const hotelsData = snapshot.val();
+        if (hotelsData) {
+          const hotelsArray = Object.keys(hotelsData).map((key) => ({
+            id: key,
+            ...hotelsData[key],
+          }));
+          setHotels(hotelsArray);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchHotels();
+  }, []);
+
   return (
     <div style={{ maxWidth: '600px', margin: '0 auto' }}>
       <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>Hotels</h2>
@@ -12,7 +38,7 @@ function HotelList({ hotels }) {
             padding: '20px',
             background: '#f8f8f8',
             borderRadius: '5px',
-            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)'
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
           }}
         >
           <h3 style={{ fontSize: '20px', marginBottom: '10px' }}>{hotel.name}</h3>
