3fa11dd40997a7f396e6dd6ec97a464a360cc1ef
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:40:58 +0300

    Update HotelForm.js

--- src/components/Admin/HotelForm.js
+++ src/components/Admin/HotelForm.js
@@ -24,10 +24,10 @@ function HotelForm({ addHotel }) {
   };
 
   return (
-    <div>
-      <h2>Add Hotel</h2>
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+      <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>Add Hotel</h2>
       <form onSubmit={handleSubmit}>
-        <label>
+        <label style={{ display: 'block', marginBottom: '10px' }}>
           Hotel Name:
           <input
             type="text"
@@ -35,9 +35,10 @@ function HotelForm({ addHotel }) {
             value={hotelData.name}
             onChange={handleInputChange}
             required
+            style={{ width: '100%', padding: '5px' }}
           />
         </label>
-        <label>
+        <label style={{ display: 'block', marginBottom: '10px' }}>
           Location:
           <input
             type="text"
@@ -45,9 +46,10 @@ function HotelForm({ addHotel }) {
             value={hotelData.location}
             onChange={handleInputChange}
             required
+            style={{ width: '100%', padding: '5px' }}
           />
         </label>
-        <label>
+        <label style={{ display: 'block', marginBottom: '10px' }}>
           Price Range:
           <input
             type="text"
@@ -55,9 +57,10 @@ function HotelForm({ addHotel }) {
             value={hotelData.priceRange}
             onChange={handleInputChange}
             required
+            style={{ width: '100%', padding: '5px' }}
           />
         </label>
-        <label>
+        <label style={{ display: 'block', marginBottom: '10px' }}>
           Hotel Photos:
           <input
             type="file"
@@ -65,9 +68,22 @@ function HotelForm({ addHotel }) {
             multiple
             onChange={handlePhotoUpload}
             required
+            style={{ width: '100%', padding: '5px' }}
           />
         </label>
-        <button type="submit">Add Hotel</button>
+        <button
+          type="submit"
+          style={{
+            padding: '8px 16px',
+            backgroundColor: '#4caf50',
+            color: 'white',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+          }}
+        >
+          Add Hotel
+        </button>
       </form>
     </div>
   );
