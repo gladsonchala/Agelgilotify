a2cb320367e463091167f97096bb16a64553b12a
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:33:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div
+      style={{
+        background: '#f5f5f5',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Arial, sans-serif',
+      }}
+    >
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
