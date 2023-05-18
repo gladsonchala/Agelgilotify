4125f29f3b456bd0f84dd8fb6101550d404f7b1d
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-18 08:38:36 +0300

    Update Map.js

--- src/components/User/Map.js
+++ src/components/User/Map.js
@@ -4,8 +4,8 @@ function Map({ hotels }) {
   // Implement map functionality using Google Maps API or any other map library of your choice
 
   return (
-    <div>
-      <h2>Map</h2>
+    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <h2 style={{ fontSize: '24px', marginBottom: '20px' }}>Map</h2>
       {/* Render the map component here */}
     </div>
   );
